7f0a8caef38d81e059f30be2bca5a498dc10b75f
kennethmokhethi <kennethmahlatsemokhethi@gmail.com>
2020-01-22 14:51:46 +0200

    Wrote main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,12 @@ public class Main {
     public static void main(String[] args)
     {
         Dice dice = new Dice(4 , new int[]{3, 4, 2});
-        System.out.println(dice.probability[0]);
+        System.out.println("Number of sides "+dice.getSides());;
+
+        System.out.println("Rolled dice" + dice.roll());
+
+        System.out.println("Weighted value" + dice.getWeighted_value());
+
+
     }
 }
